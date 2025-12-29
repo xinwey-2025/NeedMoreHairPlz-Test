1e51704cd52e8e6eae4160a53c51fb447f78ae7f
TAN WEN JIE <wenjietan035@gmail.com>
2025-12-29 23:35:26 +0800

    Call API for LogInView and SignUpView

--- src/main/java/com/journalapp/App.java
+++ src/main/java/com/journalapp/App.java
@@ -1,4 +1,4 @@
-package main.java.com.journalapp;
+package com.journalapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,8 +8,12 @@ import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws Exception {
+        // This loads the initial view defined in your resources
+        Parent root = FXMLLoader.load(getClass().getResource("/com/journalapp/view/Auth.fxml"));
+        primaryStage.setTitle("Journal Application");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
